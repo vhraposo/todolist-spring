edf6abcdbe458b56dee371549d1221fad233cfa6
vhraposo <thevhup@gmail.com>
2023-10-10 10:29:10 -0300

    feat: create user repository

--- src/main/java/br/com/vraposo/todolist/user/UserController.java
+++ src/main/java/br/com/vraposo/todolist/user/UserController.java
@@ -1,5 +1,6 @@
 package br.com.vraposo.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,11 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
     
+    @Autowired
+    private IUserRepository userRepository;
+
     @PostMapping("/")
-    public void create(@RequestBody UserModel userModel){
-          System.out.println(userModel.getUsername());
+    public UserModel create(@RequestBody UserModel userModel){
+         this.userRepository.save(userModel);
+            var userCreated = this.userRepository.save(userModel);
+            return userCreated;   
 
-
-     
     }
 }
