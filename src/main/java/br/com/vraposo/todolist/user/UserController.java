ff24ad95fc4a702fffbf5d31952f2f0172a8d748
vhraposo <thevhup@gmail.com>
2023-10-11 22:09:13 -0300

    feat: add bcrypt encrypt password at user controller

--- src/main/java/br/com/vraposo/todolist/user/UserController.java
+++ src/main/java/br/com/vraposo/todolist/user/UserController.java
@@ -1,11 +1,15 @@
 package br.com.vraposo.todolist.user;
 
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
 
 @RestController
 @RequestMapping("/users")
@@ -23,6 +27,10 @@ public class UserController {
 
             return ResponseEntity.status(400).body("Usuário já existe");
         }
+
+            var passwordHashred =  BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+
+            userModel.setPassword(passwordHashred);
 
             var userCreated = this.userRepository.save(userModel);
             return ResponseEntity.status(201).body(userCreated);   
