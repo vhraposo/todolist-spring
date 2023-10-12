ca4a00083b56a5034cdbadd64cf68c5f00411671
vhraposo <thevhup@gmail.com>
2023-10-11 22:50:49 -0300

    feat: add validade user have authorization at FilterTask

--- src/main/java/br/com/vraposo/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/vraposo/todolist/filter/FilterTaskAuth.java
@@ -3,10 +3,12 @@ package br.com.vraposo.todolist.filter;
 import java.io.IOException;
 import java.util.Base64;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import br.com.vraposo.todolist.user.IUserRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -14,6 +16,10 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @Component
 public class FilterTaskAuth extends OncePerRequestFilter {
+
+
+    @Autowired
+    private IUserRepository userRepository;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
@@ -37,10 +43,20 @@ public class FilterTaskAuth extends OncePerRequestFilter {
                 System.out.println(password);
 
              //validate user
-
-             //validate password
-
-             //next
+             var user = this.userRepository.findByUsername(username);
+             if(user == null){
+                 response.sendError(401, "Usuário sem Autorização ");
+             }else{
+                 //validate password
+                    var passwordVerify = BCrypt.verifyer().verify(password.toCharArray() , user.getPassword());
+                    if(passwordVerify.verified){
+                        filterChain.doFilter(request, response);
+                    }else{
+                        response.sendError(401);
+                    }
+                 //next
+             }
+            
     }
 
     
