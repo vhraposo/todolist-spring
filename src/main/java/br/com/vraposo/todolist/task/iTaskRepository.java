837263b203cc6980df03943bea754f89adc34a32
vhraposo <thevhup@gmail.com>
2023-10-13 00:34:21 -0300

    feat: add get tasks by user at tasks controller

--- src/main/java/br/com/vraposo/todolist/task/iTaskRepository.java
+++ src/main/java/br/com/vraposo/todolist/task/iTaskRepository.java
@@ -3,7 +3,9 @@ package br.com.vraposo.todolist.task;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
 
 public interface iTaskRepository extends JpaRepository<TaskModel, UUID> {
-    
+    List<TaskModel> findByIdUser(UUID id);
 }
