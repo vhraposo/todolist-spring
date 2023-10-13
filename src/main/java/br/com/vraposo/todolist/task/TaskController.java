154fb9108f0b993d47878775a3bc217c9283c641
vhraposo <thevhup@gmail.com>
2023-10-13 01:16:15 -0300

    feat: add null and non null properties at utils

--- src/main/java/br/com/vraposo/todolist/task/TaskController.java
+++ src/main/java/br/com/vraposo/todolist/task/TaskController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.vraposo.todolist.utils.Utils;
 import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
@@ -51,9 +52,11 @@ public class TaskController {
 
     @PutMapping("/{id}")
     public TaskModel update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id){
-        var idUser  = request.getAttribute("idUser");
-        taskModel.setIdUser( (UUID) idUser);
-        taskModel.setId(id);
-        return this.taskRepository.save(taskModel);
+        
+        var task = this.taskRepository.findById(id).orElse(null);
+
+        Utils.copyNonNullProperties(taskModel, task);
+               
+        return this.taskRepository.save(task);
     }
 }
